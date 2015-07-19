dbf9331ea2627844928df667c4dcf68ff13d523e
luzhongpeng <1234qwqw>
2015-07-19 21:33:31 +0800

    1. 删除media目录，统一归到assets 2. 调整部分目录

--- qq.php
+++
@@ -1,8 +0,0 @@
-<?php
-$qqdata = file('qq_test.qdb');
-foreach ($qqdata AS $row)
-{
-	@list($qq, $sid) = @explode(',', $row);
-	$url = 'http://pt.3g.qq.com/s?aid=nLogin3gqqbysid&3gqqsid=' . $sid;
-	echo '<div style="float:left;"><p style="text-align:center;padding:0;margin:0 0 5px 0;">'. $qq .'</p><iframe width="242" height="500" src="' . $url . '"></iframe></div>';
-}
